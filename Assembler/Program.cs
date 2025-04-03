7af146e260a303bca99a02acfdbc010505328863
jprebola <jprebola@vols.utk.edu>
2025-04-03 18:23:26 -0400

    ljl

--- Assembler/Program.cs
+++ Assembler/Program.cs
@@ -61,140 +61,139 @@ class Assembler{
             //if we are parsing an instruction then increase instruction counter
             if(isInstruction && data.Count > 0){
                 //add instruction to IInstruction list
-                if(!data[0].Equals("stpush")){
+                //TODO: this switch only checks for if and print. we need to handle to suffix of these functions somehow
+                switch(data[0]){
+                    case "exit":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
 
-                    //TODO: this switch only checks for if and print. we need to handle to suffix of these functions somehow
-                    switch(data[0]){
-                        case "exit":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
+                    case "swap":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "nop":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    case "input":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
 
-                        case "swap":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "nop":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        case "input":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
+                    case "stinput":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "debug":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "pop":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "add":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "sub":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "mul":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
 
-                        case "stinput":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "debug":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "pop":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "add":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "sub":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "mul":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
+                    case "div":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "rem":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "and":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "or":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "xor":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "lsl":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "lsr":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "asr":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "neg":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "not":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "stprint":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "call":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "return":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "goto":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "if":
+                        //Have to handle binary and uniary suffix
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "dup":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "print":
+                    //Have to handle print suffix
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "dump":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
+                    
+                    case "push":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
 
-                        case "div":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "rem":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "and":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "or":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "xor":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "lsl":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "lsr":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "asr":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "neg":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "not":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "stprint":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "call":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "return":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "goto":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "if":
-                            //Have to handle binary and uniary suffix
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "dup":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "print":
-                        //Have to handle print suffix
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "dump":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
-                        
-                        case "push":
-                            Console.WriteLine("You called: " + data[0]);
-                            break;
+                    case "stpush":
+                        Console.WriteLine("You called: " + data[0]);
+                        break;
 
-                        default:
-                            Console.WriteLine("Invalid Instruction: " + data[0]);
-                            break;
-                    }
-                    numInstructions++;
-                }else{
-                //get string length to calc # of pushes needed for PC and IInstruction list
-
-                Console.WriteLine("STPUSH FOUND");
-                }   
+                    default:
+                        Console.WriteLine("Invalid Instruction: " + data[0]);
+                        break;
+                }
+                numInstructions++;
             }
-            lines++;
+                //get string length to calc # of pushes needed for PC and IInstruction list   
+            lines++;     
         }
+        
 
         using (var stream = File.Open(args[1], FileMode.Create))
         {

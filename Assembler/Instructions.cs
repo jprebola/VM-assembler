0956bbad04a5308252a7095303af5c44a82f3887
jprebola <jprebola@vols.utk.edu>
2025-04-03 15:56:56 -0400

    progrezss!!!!!!!!!!!!!!!!

---
+++ Assembler/Instructions.cs
@@ -0,0 +1,9 @@
+public class Dup : IInstruction {
+    private readonly int _offset;
+    public Dup(int offset) {
+        _offset = offset & ~3;
+    }
+    public int Encode() {
+        return (0b1100 << 28) | _offset;
+    }
+}

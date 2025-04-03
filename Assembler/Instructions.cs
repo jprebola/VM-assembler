5ffb238efac556993b4fc3d6291c9aa1c8ab0ec9
tyler-garriott <tyler.garriott@gmail.com>
2025-04-03 16:37:52 -0400

    push

--- Assembler/Instructions.cs
+++ Assembler/Instructions.cs
@@ -6,4 +6,9 @@ public class Dup : IInstruction {
     public int Encode() {
         return (0b1100 << 28) | _offset;
     }
+    public int push(int value) {
+        int opcode = 0b1111 << 28;
+        int masked = value & 0x0FFFFFFF; // Keep lower 28 bits
+        return opcode | masked;
+    }
 }

bc7ee54a610b9bfae6ee154b6e122e87a68bd758
cheyennehooper <hooptown52@gmail.com>
2025-04-04 20:50:09 -0400

    call shifting and proper label parsing

--- Assembler/Instructions.cs
+++ Assembler/Instructions.cs
@@ -110,10 +110,10 @@ public class Debug : IInstruction {
 public class Call : IInstruction {
     private int _offset;
     public Call(int labelPos, int currentPos){
-        _offset = labelPos - currentPos;
+        _offset = (labelPos - currentPos) >> 2;  //divide by 4 for instruction addresses
     }
     public int Encode(){
-        return (0b0101 << 28) | (_offset << 2);
+        return (0b0101 << 28) | (_offset & 0x0FFFFFFF);  //mask to keep only 28 bits
     }
 }
 
